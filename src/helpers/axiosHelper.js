7a83cef64b4ed3110814a3c6293dd0bcf37c3527
sarvesh rajpure <rajpuresarvesh@gmail.com>
2024-02-01 23:10:45 +0530

    fixed axios helper

--- src/helpers/axiosHelper.js
+++ src/helpers/axiosHelper.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    process.env.REACT_APP_ENV === "dev" ? `${process.env.REACT_APP_LOCAL_SETUP_BASE_URL}/api` : "",
+    process.env.REACT_APP_ENV === "dev"
+      ? `${process.env.REACT_APP_LOCAL_SETUP_BASE_URL}/api`
+      : "/api",
   withCredentials: true,
 });
 axios.defaults.headers.post["Content-Type"] = "application/json";
